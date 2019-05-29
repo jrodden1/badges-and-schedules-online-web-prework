7171d71ab8e04db64b477e96ef8432791e7a845d
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 20:45:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakerArr)
+  batch_of_badges = speakerArr.map { |speaker| badge_maker(speaker)}
+end
+
+def assign_rooms(speakerArr)
+  room_assignments = speakerArr.map { |speaker| }
+
+end
