2d48518c8c56442168de64e4aad7533c15db94af
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 21:12:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,12 +3,14 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speakerArr)
-  batch_of_badges = speakerArr.map { |speaker| badge_maker(speaker)}
+  batch_of_badges = speakerArr.map do |speaker| 
+    badge_maker(speaker)
+  end
 end
 
 def assign_rooms(speakerArr)
-  room_number = 1
-  room_assignments = speakerArr.map do { |speaker|
-    "Hello, #{speaker}! You'll be assigned to room #{room_number}." room_number =+ 1}
+  room_assignments = speakerArr.map.with_index do |speaker, index|
+      "Hello, #{speaker}! You'll be assigned to room #{index + 1}."
+  end
 
 end
